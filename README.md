3be34e947b7bdf055c9e96d261b7fa8a18f5093e
louipasquini <louipasquini@gmail.com>
2025-11-07 20:58:34 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Punklab WIP
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Paper from './assets/paper.png'
+import PunkLabLogo from './assets/PunkLab.png'
+
+function App() {
+
+  return (
+    <div className="app">
+      <img src={Paper} alt="?" className="paper-left" />
+      <img className="logo" src={PunkLabLogo} alt="PunkLab Logo" />
+      <h3 className="wip">⚒️<br></br>em construção...</h3>
+      <h3 className="wip sub">por @347loui</h3>
+      <img src={Paper} alt="?" className="paper-right" />
+    </div>
+  )
+}
+
+export default App
