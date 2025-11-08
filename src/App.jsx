b180c2b678bd85374adccba24f86ec17b4545c89
louipasquini <louipasquini@gmail.com>
2025-11-07 21:27:54 -0300

    another big mistake

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="app">
       <img src={Paper} alt="?" className="paper-left" />
-      <img className="logo" src={PunkLabLogo} alt="PunkLab Logo" />
       <h3 className="wip">⚒️<br></br>em construção...</h3>
+      <img className="logo" src={PunkLabLogo} alt="PunkLab Logo" />
       <h3 className="wip sub">por @347loui</h3>
       <img src={Paper} alt="?" className="paper-right" />
     </div>
